854f4894cec97372ecae50a6185655801469a9c5
agroedulabs <agroedulabs@gmail.com>
2026-04-18 11:05:29 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Mantra agar Vercel tidak mogok kalau ada error tipe data
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Mantra agar Vercel tidak cerewet soal aturan penulisan kode
     ignoreDuringBuilds: true,
   },
 };
