a04130fd0410654a3854bad83086622642e7302e
agroedulabs <agroedulabs@gmail.com>
2026-04-18 09:25:52 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "./globals.css"; // Baris ini WAJIB ada di paling atas
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
